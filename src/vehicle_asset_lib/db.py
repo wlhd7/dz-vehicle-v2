f83711aed2c29fcbdc32d9d0d29478f4557e52b0
wlhd7 <3509896163@qq.com>
2026-02-27 10:09:56 +0800

    fix(db): support DATABASE_URL env var for data persistence

--- src/vehicle_asset_lib/db.py
+++ src/vehicle_asset_lib/db.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from .models import Base
 
-DATABASE_URL = "sqlite:///vehicle_asset.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///vehicle_asset.db")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
